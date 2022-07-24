779f9f0d3677e8dff7d5c7ff7a803b97381028f5
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-07-23 23:26:25 -0500

    Solucion Ruby_Detectar_Substring_OBS
    Observaciones

---
+++ Ruby_Detectar_Substring_OBS.rb
@@ -0,0 +1,13 @@
+#shipping method
+def shipping(address)
+  result = "We only ship orders to Mexico"
+  address.split(" ").each {|word| result = "Order received" if word.downcase =="mexico" or word.downcase =="mèxico" }
+  result
+end
+
+#driver code
+
+p shipping('Insurgentes Sur 8932, Alvaro Obregon, Mexico') == "Order received"
+p shipping('Insurgentes Sur 8932, Alvaro Obregon, MÈxIcO') == "Order received"
+p shipping('Geary Blvd 3320, San Francisco, Estados Unidos') == "We only ship orders to Mexico"
+
