961c53342cca185130a2353cb91d02ee2f5c9d40
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-07-29 14:48:01 -0500

    Solucionado Ruby_String_mas_Largo_OBS2
    Se levantaron observaciones de Ruby_String_mas_Largo_OBS2

---
+++ Ruby_String_mas_Largo_OBS2.rb
@@ -0,0 +1,43 @@
+#longest method
+def longest(mi_array)
+  max = mi_array[0].length
+  result = []
+  mi_array.each {|dato| max = dato.length if dato.length > max}
+  mi_array.each {|dato| result << dato if dato.length == max}
+  result
+end
+
+"""Metodo Enumerable"""
+def longest2(mi_array)
+  max = mi_array[0].length
+  result = []
+  mi_array.each {|dato| max = dato.length if dato.length > max}
+  mi_array.select {|arr| arr.length == max}
+end
+
+#Driver code
+
+p longest(['tres', 'pez', 'alerta', 'cuatro', 'tesla', 'tropas', 'siete']) == ["alerta", "cuatro", "tropas"]
+p longest(['gato', 'perro', 'elefante', 'jirafa']) == ["elefante"]
+p longest(['verde', 'rojo', 'negro', 'morado']) == ["morado"]
+
+p "Metodo Enumerable"
+p longest2(['tres', 'pez', 'alerta', 'cuatro', 'tesla', 'tropas', 'siete']) == ["alerta", "cuatro", "tropas"]
+p longest2(['gato', 'perro', 'elefante', 'jirafa']) == ["elefante"]
+p longest2(['verde', 'rojo', 'negro', 'morado']) == ["morado"]
+
+
+=begin
+
+- ¿Cuál es la opción rubista del método 'push'?
+Se puede usar el push o el append pero el metodo rubista seria "<<""
+
+
+- ¿Es necesario usar la variable 'result'?
+No, se elimino
+
+
+- ¿Para aplicar el método 'select' es necesario volver a llamar al arreglo 'mi_array?
+No, se corrigio
+
+=end
