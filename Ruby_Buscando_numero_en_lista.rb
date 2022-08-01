ba790ce7e224c8afcc49465fc5e7105a89a155d6
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-07-31 21:31:28 -0500

    Solucionado Ruby_Buscando_numero_en_lista

---
+++ Ruby_Buscando_numero_en_lista.rb
@@ -0,0 +1,12 @@
+#numbers method
+
+def numbers(lista, number)
+  lista.include?(number)
+end
+
+#Driver code
+
+p numbers([1, 3, 5, 7, 9, 11], 5) == true
+p numbers([1, 50, 24, 7, 9, 100], 25) == false
+p numbers([11, 33, 55, 77, 99, 11], 33) == true
+p numbers([20, 43, 55, 77, 99, 40], 43) == true
