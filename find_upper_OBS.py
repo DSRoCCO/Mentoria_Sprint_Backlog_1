e5f854b706b1335ec772ad26af90429b0d0bc3b4
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-08-21 19:25:33 -0500

    Solucionado Python_Detectando_mayuscula OBS

---
+++ find_upper_OBS.py
@@ -0,0 +1,14 @@
+"""Ejemplo de resultado"""
+def find_upper(person1, person2):
+    persons = [person1, person2]
+    result = {}
+    for person in persons:
+        if person['last_name'][0] == person['last_name'][0].upper():
+            result[person['last_name'][0]]= person['last_name']
+    return result
+
+#person1 = {"name": "candy", "last_name": "Feder", "age": 34}
+#person2 = {"name": "Manuel", "last_name": "martínes", "age": 21}
+
+#print(find_upper(person1, person2))
+#>> {'F': 'Feder'}
