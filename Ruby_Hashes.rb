5dabb3209575fbe62d52b7df60cc15e25adf94f8
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-09-05 08:57:43 -0500

    Solucionado Ruby_Hashes

---
+++ Ruby_Hashes.rb
@@ -0,0 +1,24 @@
+
+#Variable 'new_hash'
+new_has = Hash.new
+
+#Variable 'canasta_de_frutas'
+
+canasta_de_frutas = {
+  "platano" => 5,
+  "fresa" => 6,
+  "papaya" => 2
+}
+
+#Agregar una fruta
+
+canasta_de_frutas["uva"] = 12
+
+
+#Variable 'fruta seleccionada'
+
+fruta_selecccionada = canasta_de_frutas["fresa"]
+p fruta_selecccionada
+
+
+
