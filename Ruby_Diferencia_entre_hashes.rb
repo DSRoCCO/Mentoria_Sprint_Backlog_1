40e387ea68eb7fa1e8adba9ff4e1b3411e457209
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-09-05 09:00:03 -0500

    Solucionado Ruby_Diferencia_entre_hashes

---
+++ Ruby_Diferencia_entre_hashes.rb
@@ -0,0 +1,18 @@
+key = "Lets Code"
+hash = {key: "The Future"}
+hash2 = {key => "The Future"}
+
+
+¿Cuál es la diferencia entre los dos hashes?
+
+El la primera variable es una forma de declarar un variable tipo hash.
+La forma de declarar la variable hash2 es con la palabra key como simbolo :key o como string 'key', de la siguiente forma:
+hash2 = {:key => "The Future"}
+hash2 = {"key" => "The Future"}
+
+Si ambos variables hash se ingresan correctamente, ambas sera guardadas como
+hash = {:key =>"The Future"}
+hash2 = {:key => "The Future"}
+
+
+
