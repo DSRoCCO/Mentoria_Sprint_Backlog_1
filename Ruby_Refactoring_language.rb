bb9c0dbb4a9cbbd2a558de3e65529e15c6e02b88
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-09-18 21:03:39 -0500

    Solucionado Ruby--Refactoring-language

---
+++ Ruby_Refactoring_language.rb
@@ -0,0 +1,15 @@
+# Ejercicio - Refactoring language
+
+def language(speak)
+  lenguajes = ["Javascript", "CSS", "Python", "R", "C++", "Unity", "Ruby", "Rails", "Java"]
+  p lenguajes.include?(speak.capitalize!) ? "I like #{speak}": "I don't like to code"
+end
+# Driver code
+
+p language("Javascript") == "I like Javascript"
+p language("Unity") == "I like Unity"
+p language("Swift") == "I don't like to code"
+p language("Kotlin") == "I don't like to code"
+p language("Android") == "I don't like to code"
+p language("ruby") == "I like Ruby"
+p language("rails") == "I like Rails"
