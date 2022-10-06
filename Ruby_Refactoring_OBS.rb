b3ca413db534c30f713607bd10120c846e7268b1
Roger Christian Cansaya Olazabal <67093038+DSRoCCO@users.noreply.github.com>
2022-10-06 17:51:21 -0500

    Solucionado Ruby-Refactoring

---
+++ Ruby_Refactoring_OBS.rb
@@ -0,0 +1,10 @@
+# Refactoriza el siguiente código:
+
+def age(num)
+"Woow... You are #{num} years old" if num > 0
+end
+
+# Driver code
+p age(19) == "Woow... You are 19 years old"
+p age(24) == "Woow... You are 24 years old"
+p age(32) == "Woow... You are 32 years old"
